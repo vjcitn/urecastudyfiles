4d447d84946f6fa72a6952f5de51e5bd1e7bb247
Sammi-Smith <49876975+Sammi-Smith@users.noreply.github.com>
2024-05-21 16:40:35 -0400

    Add password to app

--- streamlit_ureca_app.py
+++ streamlit_ureca_app.py
@@ -5,6 +5,34 @@ from frictionless import Resource, Dialect, formats
 import sqlite3
 import json
 
+import hmac
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
 st.set_page_config(
     page_title="SDY1644 (URECA) Study Files",
     page_icon="🫁",

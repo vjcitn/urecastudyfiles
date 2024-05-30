6dc3e714ca241858f2055911d2c67bbd72b67600
Sammi-Smith <49876975+Sammi-Smith@users.noreply.github.com>
2024-05-30 08:47:09 -0400

    Add advanced search and filter features

--- streamlit_ureca_app.py
+++ streamlit_ureca_app.py
@@ -5,6 +5,16 @@ from frictionless import Resource, Dialect, formats
 import sqlite3
 import json
 
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
+import warnings
+from random import choice
+from string import digits
+
 import hmac
 
 def check_password():
@@ -32,6 +42,110 @@ def check_password():
 
 if not check_password():
     st.stop()  # Do not continue if check_password is not True.
+
+
+def dataframe_explorer(df: pd.DataFrame, case: bool = True) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+
+    Args:
+        df (pd.DataFrame): Original dataframe
+        case (bool, optional): If True, text inputs will be case sensitive. Defaults to True.
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+
+    if df.empty: #this prevents key collisions when multiple empty dataframes exist, since they won't have unique hash values
+        random_key_base = ''.join(choice(digits) for i in range(12))
+    else:
+        random_key_base = pd.util.hash_pandas_object(df)
+
+    modify = st.checkbox("Add filters", key=f"{random_key_base}_checkbox")
+
+    if not modify:
+        return df
+
+    df = df.copy()
+
+    # Try to convert datetimes into standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                with warnings.catch_warnings(): #suppresses: UserWarning: Could not infer format, so each element will be parsed individually, falling back to `dateutil`. To ensure parsing is consistent and as-expected, please specify a format.
+                    warnings.simplefilter("ignore")
+                    df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect(
+            "Filter dataframe on",
+            df.columns,
+            key=f"{random_key_base}_multiselect",
+        )
+        filters: Dict[str, Any] = dict()
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            # Treat columns with < 10 unique values as categorical
+            # if is_categorical_dtype(df[column]) or df[column].nunique() < 10: #is_categorical_dtype() is deprecated
+            if isinstance(df[column].dtype, pd.CategoricalDtype) or df[column].nunique() < 10:
+                left.write("↳")
+                if df[column].notnull().all():
+                    df_column = df[column] #don't attempt to fillna if there are none, because this leads to an error with categorial columns
+                else:
+                    df_column = df[column].fillna("None") #must fill missing values, because multiselect widget errors out with nan values present
+
+                filters[column] = right.multiselect(
+                    f"Values for {column}",
+                    df_column.unique(), 
+                    default=list(df_column.unique()),
+                    key=f"{random_key_base}_{column}",
+                )
+                df = df[df_column.isin(filters[column])]
+            elif is_numeric_dtype(df[column]):
+                left.write("↳")
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                filters[column] = right.slider(
+                    f"Values for {column}",
+                    _min,
+                    _max,
+                    (_min, _max),
+                    step=step,
+                    key=f"{random_key_base}_{column}",
+                )
+                df = df[df[column].between(*filters[column])]
+            elif is_datetime64_any_dtype(df[column]):
+                left.write("↳")
+                filters[column] = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                    key=f"{random_key_base}_{column}",
+                )
+                if len(filters[column]) == 2:
+                    filters[column] = tuple(map(pd.to_datetime, filters[column]))
+                    start_date, end_date = filters[column]
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                left.write("↳")
+                filters[column] = right.text_input(
+                    f"Search in {column}",
+                    key=f"{random_key_base}_{column}",
+                )
+                if filters[column]:
+                    df = df[df[column].str.contains(filters[column], case=case, na = False)]
+
+    return df
 
 st.set_page_config(
     page_title="SDY1644 (URECA) Study Files",
@@ -65,6 +179,14 @@ cache_ai_df = pd.DataFrame(fetched_ai, columns = ["file_name", "generated_md5", 
 
 study_file_df_ureca_with_ai_generated = pd.merge(left = study_file_df_ureca, right = cache_ai_df, left_on = "fileName", right_on = "file_name")
 
+#create one big df with both metadata and AI generated file details
+study_file_df_ureca_with_all_file_details = pd.merge(left = study_file_df_ureca, right = cache_ai_df, left_on = "fileName", right_on = "file_name", how = "left")
+study_file_df_ureca_with_all_file_details = pd.merge(left = study_file_df_ureca_with_all_file_details, right = cache_df, left_on = "file_name", right_on = "file_name", how = "left")
+categorical_cols = ["studyFileType"]
+study_file_df_ureca_with_all_file_details[categorical_cols] = study_file_df_ureca_with_all_file_details[categorical_cols].astype('category')
+
+study_file_df_ureca_with_all_file_details["metadata_available"] = [False if pd.isnull(row) else "result" not in json.loads(row) for row in study_file_df_ureca_with_all_file_details["metadata"]] 
+
 #create file and corresponding data dictionary mapping
 study_file_df_ureca_data_dictionaries = study_file_df_ureca[study_file_df_ureca["studyFileType"] == "Data Dictionary"]
 data_dictionaries_list = study_file_df_ureca_data_dictionaries[study_file_df_ureca_data_dictionaries["fileName"].str.endswith("_dictionary.txt")]["fileName"].to_list()
@@ -84,6 +206,13 @@ for data_dictionary in data_dictionaries_list:
 #----------
 st.header("SDY1644 (URECA) Study Files", divider=True)
 st.dataframe(study_file_df_ureca[["fileName", "studyFileType", "description"]], hide_index=True, use_container_width=False)
+st.divider()
+
+
+st.subheader('Search and Filter Files')
+filtered_df = dataframe_explorer(study_file_df_ureca_with_all_file_details[["fileName", "studyFileType", "description", "ai_generated_keywords", "ai_generated_summary", "metadata", "metadata_available"]], case=False)
+st.write(len(filtered_df), "matching files found")
+st.dataframe(filtered_df, hide_index=True, use_container_width=True)
 st.divider()
 
 st.header("File Details", divider=True)
@@ -143,14 +272,18 @@ else:
         with Resource(base_path + selected_file_name, dialect = Dialect(skip_blank_rows=True)) as resource:
                 
             extracted_data = pd.DataFrame(resource.read_rows())
-            st.dataframe(extracted_data, hide_index=True, use_container_width=True)
+            extracted_data_filtered = dataframe_explorer(extracted_data, case=False)
+            st.write(len(extracted_data_filtered), "matching rows found")
+            st.dataframe(extracted_data_filtered, hide_index=True, use_container_width=True)
 
     elif selected_file_name.lower().endswith(".txt"):
         
         with Resource(base_path + selected_file_name, format='tsv', dialect = Dialect(skip_blank_rows=True)) as resource:
         
             extracted_data = pd.DataFrame(resource.read_rows())
-            st.dataframe(extracted_data, hide_index=True, use_container_width=True)
+            extracted_data_filtered = dataframe_explorer(extracted_data, case=False)
+            st.write(len(extracted_data_filtered), "matching rows found")
+            st.dataframe(extracted_data_filtered, hide_index=True, use_container_width=True)
 
     elif (selected_file_name.lower().endswith(".xlsx") or selected_file_name.lower().endswith(".xlsm") or selected_file_name.lower().endswith(".xls")):
 
@@ -165,7 +298,9 @@ else:
                 with Resource(base_path + selected_file_name, control = formats.ExcelControl(sheet=sheet), dialect = Dialect(skip_blank_rows=True)) as resource:
 
                     extracted_data = pd.DataFrame(resource.read_rows())
-                    st.dataframe(extracted_data, hide_index=True, use_container_width=True)
+                    extracted_data_filtered = dataframe_explorer(extracted_data, case=False)
+                    st.write(len(extracted_data_filtered), "matching rows found")
+                    st.dataframe(extracted_data_filtered, hide_index=True, use_container_width=True)
 
             
 #need to first check if selected file is data dict, corresponding file, or neither and only print relevent section for each of those 3
